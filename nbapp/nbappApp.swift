d32593130cc8fad1aea02709b999932d2819bce5
Merouane Bellaha <m.bellaha@gmail.com>
2022-02-02 20:47:23 +0100

    Initial Commit

---
+++ nbapp/nbappApp.swift
@@ -0,0 +1,17 @@
+//
+//  nbappApp.swift
+//  nbapp
+//
+//  Created by Merouane Bellaha on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct nbappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
